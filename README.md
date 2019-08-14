8b4b47953607254db10860ea15bfc52e4710b287
Chubi Adama <adamachubi@gmail.com>
2019-08-15 00:47:02 +0100

    added player board cell and the other methods

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# Tic-Tac-Toe
+# TIC TAC TOE
+
+
+
+
+
+## Author
+[Danilo Zagarcanin ](https://github.com/danilozag1992)
+[Adama Chubiyojo Desmond](https://github.com/kobiyoyo)
---
+++ lib/player.rb
@@ -0,0 +1,8 @@
+class Player
+    attr_reader :choice, :name
+
+    def initialize(params)
+        @choice = params.fetch(:choice)
+        @name = params.fetch(:name)
+    end
+end
