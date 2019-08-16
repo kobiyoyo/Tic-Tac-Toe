d1d5f3e87a076c2aa4274fca7d6b03bc2ea1e237
danilozag1992 <danilozagarcanin@gmail.com>
2019-08-16 12:21:46 +0200

    added game logic

---
+++ lib/cell.rb
@@ -0,0 +1,7 @@
+class Cell
+  attr_accessor :value
+
+  def initialize(value = '')
+    @value = value
+  end
+end
---
+++ lib/my_array.rb
@@ -0,0 +1,17 @@
+class Array
+  def all_empty?
+    all? { |x| x.to_s.empty? }
+  end
+
+  def all_same?
+    all? { |x| x == self[0] }
+  end
+
+  def any_empty?
+    any? { |x| x.to_s.empty? }
+  end
+
+  def none_empty?
+    !any_empty?
+  end
+end
